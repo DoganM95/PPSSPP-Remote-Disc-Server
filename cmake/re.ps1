11245e025b9bb4ae6b82d6927abb20e730e4f5f2
Dogan <doganmermertas@hotmail.de>
2021-03-31 01:07:27 +0200

    Added warning and container remover

--- cmake/re.ps1
+++ cmake/re.ps1
@@ -1,3 +1,5 @@
+# Use this file with caution!. Will kill and delete any existing container, then start ppsspp
 docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
 docker build -t ppsspp .
 docker run -p 8300:8300 -v "C:\Users\Dogan\OneDrive\Desktop\:/root/.config/ppsspp/PSP/GAME/" ppsspp
