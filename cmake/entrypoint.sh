28ef74857bb48e69d0b637a64a6670778295a3ae
Dogan <doganmermertas@hotmail.de>
2021-03-31 22:47:17 +0200

    Added ampersand escape for game titles

--- cmake/entrypoint.sh
+++ cmake/entrypoint.sh
@@ -12,7 +12,7 @@ success_output_1_identifier="loading control pad mappings from gamecontrollerdb.
 success_output_2_identifier="ALSA: Couldn't open audio device"
 failure_output_1_identifier="Unable to initialize SDL"
 
-update_interval=5 # The higher the interval, the faster it updates the lib on change, but also increases cpu usage
+update_interval=20 # The higher the interval, the faster it updates the lib on change, but also increases cpu usage
 
 function main() {
     # Start PPSSPP app initially to initialize a default ppsspp.ini file
@@ -22,24 +22,24 @@ function main() {
     while true; do
         file_counter=0
 
-        sed -Ei "s/(MaxRecent =) (.*)/\1 10000000/" $ppsspp_ini_file         # MaxRecent
-        sed -Ei "s/(RemoteISOManualConfig =) (.*)/\1 True/" $ppsspp_ini_file # RemoteISOManualConfig
-        sed -Ei "s/(RemoteISOPort =) (.*)/\1 8300/" $ppsspp_ini_file         # RemoteISOPort
-        sed -Ei "s/(RemoteShareOnStartup =) (.*)/\1 True/" $ppsspp_ini_file  # RemoteShareOnStartup
-        sed -Ei "s/(RunCount =) (.*)/\1 1337/" $ppsspp_ini_file              # RunCount
+        sed -Ei "s|(MaxRecent =) (.*)|\1 10000000|" $ppsspp_ini_file         # MaxRecent
+        sed -Ei "s|(RemoteISOManualConfig =) (.*)|\1 True|" $ppsspp_ini_file # RemoteISOManualConfig
+        sed -Ei "s|(RemoteISOPort =) (.*)|\1 8300|" $ppsspp_ini_file         # RemoteISOPort
+        sed -Ei "s|(RemoteShareOnStartup =) (.*)|\1 True|" $ppsspp_ini_file  # RemoteShareOnStartup
+        sed -Ei "s|(RunCount =) (.*)|\1 1337|" $ppsspp_ini_file              # RunCount
 
         sed -ir '/^\s*$/d' $ppsspp_ini_file # Remove all empty lines
 
         # Preperations for library updates
         grep -E "^FileName.*" $ppsspp_ini_file >$recents_game_list                  # Copy recent games from ppsspp.ini to a new file
-        sed -Ei "s&(FileName.* = )(.*)&\2&" $recents_game_list                      # Remove "FileNameX = " prefixes in said file
+        sed -Ei "s|(FileName.* = )(.*)|\2|" $recents_game_list                      # Remove "FileNameX = " prefixes in said file
         find "/root/.config/ppsspp/PSP/GAME/" -maxdepth 1 -print >$volume_file_list # Create a list of files in mounted volume (game folder)
         grep -E "$game_matching_regex" "$volume_file_list" >$volume_game_list       # Copy lines matching game-names to a new file
 
         while read -d $'\n' recent_game; do # Iterate over games listed under [Recent] in ppsspp.ini
             # if recent games contain a file not contained in games volume, delete all recent games to trigger a recreation
             if [ $(grep -Fc "$recent_game" "$volume_game_list") -eq 0 ]; then
-                echo "DETECTED DELETEDD GAME: $recent_game"
+                echo "DETECTED DELETEDD GAME: '$recent_game'"
                 settings_changed_flag=1
             fi
         done <$recents_game_list
@@ -55,10 +55,7 @@ function main() {
         if [[ "$settings_changed_flag" -eq 1 ]]; then # Actions on game addition/removal
             echo "Updating game library..."
             delete_gamelist
-            while read -d $'\n' file; do
-                create_gamelist "$file"
-            done <$volume_game_list
-
+            create_gamelist "$volume_game_list"
             restart_ppsspp_successfully # Restart PPSSPP to apply game library update
             settings_changed_flag=0     # Reset flag
             echo "Done."
@@ -69,12 +66,15 @@ function main() {
 }
 
 function create_gamelist() {
-    sed -i "s|MaxRecent = .*|&\nFileName$file_counter = $1|g" $ppsspp_ini_file # Add game to ppsspp-ini recents
-    file_counter=$((file_counter + 1))                                         # counter++ for next game to be added
+    while read -d $'\n' file; do
+        escaped_filename=$(echo "$file" | awk '{ sub("&", "\\\&"); print }')                      # Escape "&" (Ampersand) in game names
+        sed -i "s|MaxRecent = .*|&\nFileName$file_counter = $escaped_filename|g" $ppsspp_ini_file # Add game to ppsspp-ini recents
+        file_counter=$((file_counter + 1))                                                        # counter++ for next game to be added
+    done <$1
 }
 
 function delete_gamelist() {
-    sed -iq "s/FileName.*//g" $ppsspp_ini_file # Removing any existing recent games (FileNameX) from ppsspp.ini
+    sed -iq "s|FileName.*||g" $ppsspp_ini_file # Removing any existing recent games (FileNameX) from ppsspp.ini
     sed -ir '/^\s*$/d' $ppsspp_ini_file        # Remove all empty lines
 }
 
@@ -83,7 +83,7 @@ function kill_ppsspp() {
     pkill Xvfb
     pkill PPSSPPSDL
     while [[ $(pgrep -fi "xvfb") || $(pgrep -fi "PPSSPPSDL") ]]; do # Wait for PPSSPP process to terminate
-        echo "Waiting for PPSSPPSDL and xvfb to terminate.."
+        # echo "Waiting for PPSSPPSDL and xvfb to terminate.."
         sleep 1
     done
     rm -f /tmp/.X99-lockS # Remove any file-locks of Xvfb
@@ -94,12 +94,12 @@ function restart_ppsspp() {
     kill_ppsspp
 
     # Restart PPSSPP
-    echo "Restarting PPSSPP (xvfb & PPSSPPSDL)"
+    # echo "Restarting PPSSPP (xvfb & PPSSPPSDL)"
     echo "" >$run_log # Clear log of previous run results
     nohup xvfb-run --server-args="-screen 0, 1024x680x24" "/usr/src/app/build/PPSSPPSDL" >$run_log &# Start PPSSPP asynchronously
 
     # Wait until PPSSPP startup finishes (by waiting for log to contain an identifier)
-    echo "Waiting for log to contain an identifier.."
+    # echo "Waiting for log to contain an identifier.."
     while [[ ! $(grep -F "$success_output_1_identifier" $run_log) && ! $(grep -F "$success_output_2_identifier" $run_log) && ! $(grep -F "$failure_output_1_identifier" $run_log) ]]; do
         sleep 1
     done
@@ -111,7 +111,7 @@ function restart_ppsspp() {
         ppsspp_start_result=1
     fi
     if [[ $(grep -F "$failure_output_1_identifier" $run_log) ]]; then
-        echo "Failed to start ppsspp, restarting..."
+        # echo "Failed to start ppsspp, restarting..."
         ppsspp_start_result=0
     fi
 }
