663cb5943611147b1ff9a631821244e44c37908d
Dogan <doganmermertas@hotmail.de>
2021-04-18 17:39:36 +0200

    reformatted

--- nodejs/app.js
+++ nodejs/app.js
@@ -16,60 +16,60 @@ const PORT = process.env.PORT || 3000;
 const rewrites = flattenDirToRewrites(PATH);
 
 const serve = serveStatic(PATH, {
-	fallthrough: false,
+    fallthrough: false,
 });
 
 const server = http.createServer((request, response) => {
-	if (/^\/($|\?)/.test(request.url)) {
-		return handleListing(rewrites, response);
-	}
+    if (/^\/($|\?)/.test(request.url)) {
+        return handleListing(rewrites, response);
+    }
 
-	for (const rewrite of rewrites) {
-		if (request.url === rewrite.source) {
-			request.url = rewrite.destination;
-			break;
-		}
-	}
-	return serve(request, response, finalhandler(request, response));
+    for (const rewrite of rewrites) {
+        if (request.url === rewrite.source) {
+            request.url = rewrite.destination;
+            break;
+        }
+    }
+    return serve(request, response, finalhandler(request, response));
 });
 
 server.listen(PORT, HOST, () => {
-	console.log(`Running at http://${HOST}:${PORT}`);
+    console.log(`Running at http://${HOST}:${PORT}`);
 });
 
 /**
- * 
  * @param {Rewrite[]} rewrites List of supported files.
  * @param {http.ServerResponse} response HTTP response.
  */
 function handleListing(rewrites, response) {
-	response.setHeader('Content-Type', 'text/plain');
-	response.writeHead(200);
-	response.end('/\n' + rewrites.map(rewrite => rewrite.source).join('\n'));
+    response.setHeader('Content-Type', 'text/plain');
+    response.writeHead(200);
+    response.end('/\n' + rewrites.map(rewrite => rewrite.source).join('\n'));
 }
 
 /**
  * Read all files recursively in a folder, and extract playable files.
- *
  * These are then mapped to rewrites for serve.
- *
  * @param {String} dirPath Local path.
  * @param {String} subdir Public path so far.
  * @param {Rewrite[]} list Rewrites.
  * @returns {Rewrite[]} List of rewrites to any files.
  */
 function flattenDirToRewrites(dirPath, subdir = '/', list = []) {
-	const files = fs.readdirSync(dirPath);
+    const files = fs.readdirSync(dirPath);
 
-	return files.reduce((list, file) => {
-		const path = dirPath + '/' + file;
-		if (fs.statSync(path).isDirectory()) {
-			return flattenDirToRewrites(path, subdir + file + '/', list);
-		}
+    return files.reduce((list, file) => {
+        const path = dirPath + '/' + file;
+        if (fs.statSync(path).isDirectory()) {
+            return flattenDirToRewrites(path, subdir + file + '/', list);
+        }
 
-		if (/\.(cso|iso|pbp|elf|prx|ppdmp)$/i.test(file)) {
-			list.push({ source: '/' + encodeURIComponent(file), destination: subdir + encodeURIComponent(file) });
-		}
-		return list;
-	}, list);
+        if (/\.(cso|iso|pbp|elf|prx|ppdmp)$/i.test(file)) {
+            list.push({
+                source: '/' + encodeURIComponent(file),
+                destination: subdir + encodeURIComponent(file)
+            });
+        }
+        return list;
+    }, list);
 }
