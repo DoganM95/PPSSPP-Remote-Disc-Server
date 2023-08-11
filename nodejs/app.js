6a52a8d0473f25cbedb8c89edd121398cc1ea02f
Dogan <doganmermertas@hotmail.de>
2023-08-11 12:40:58 +0200

    Remove doc

--- nodejs/app.js
+++ nodejs/app.js
@@ -7,12 +7,6 @@ const PATH = process.env.ISO_PATH || '.';
 const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 8300;
 
-/**
- * @typedef {Object} Rewrite
- * @property {String} source Path used by PPSSPP.
- * @property {String} destination Path to actual file.
- */
-
 const serve = serveStatic(PATH, {
     fallthrough: false,
 });
@@ -37,24 +31,12 @@ server.listen(PORT, HOST, () => {
     console.log(`Running at http://${HOST}:${PORT}`);
 });
 
-/**
- * @param {Rewrite[]} rewrites List of supported files.
- * @param {http.ServerResponse} response HTTP response.
- */
 function handleListing(rewrites, response) {
     response.setHeader('Content-Type', 'text/plain');
     response.writeHead(200);
     response.end('/\n' + rewrites.map(rewrite => rewrite.source).join('\n'));
 }
 
-/**
- * Read all files recursively in a folder, and extract playable files.
- * These are then mapped to rewrites for serve.
- * @param {String} dirPath Local path.
- * @param {String} subdir Public path so far.
- * @param {Rewrite[]} list Rewrites.
- * @returns {Rewrite[]} List of rewrites to any files.
- */
 function flattenDirToRewrites(dirPath, subdir = '/', list = []) {
     const files = fs.readdirSync(dirPath);
 
