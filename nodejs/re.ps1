72983c560757c8ffbbfed0679a8661f5a9edff7d
DoganM95 <doganmermertas@hotmail.de>
2024-02-01 16:47:40 +0100

    improve helping script

--- nodejs/re.ps1
+++ nodejs/re.ps1
@@ -1,5 +1,17 @@
 # Use this file with caution!. Will kill and delete any existing container, then start ppsspp
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker build -t doganm95/ppsspp-remote-disc-server .
-docker run -p 8300:8300 -v "C:\Users\Dogan\OneDrive\Desktop\:/var/isos/" doganm95/ppsspp-remote-disc-server
+docker ps -a --format "{{.Names}}" | Where-Object { $_ -like "*ppsspp-remote-disc-server*" } | ForEach-Object { docker stop $_ }
+
+# Delete any existing rds docker images
+docker ps -a --format "{{.Names}}" | Where-Object { $_ -like "*ppsspp-remote-disc-server*" } | ForEach-Object { docker stop $_; docker rm $_ }; docker images --format "{{.Repository}}:{{.Tag}}" | Where-Object { $_ -like "*ppsspp-remote-disc-server*" } | ForEach-Object { docker rmi $_ }
+
+# Build the image locally from source
+docker build -t ppsspp-remote-disc-server .
+
+# Start a container
+docker run `
+    -d `
+    --restart always `
+    --name doganm95-ppsspp-remote-disc-server `
+    -p 8300:8300 `
+    -v "C:\Users\User\OneDrive\Desktop\iso:/var/isos/" `
+    ppsspp-remote-disc-server
